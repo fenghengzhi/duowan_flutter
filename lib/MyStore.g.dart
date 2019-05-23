7bcb0b673edbafd474fda2539a07902ad3273b67
hzfeng <hzfeng@Ctrip.com>
2019-05-23 20:24:04 +0800

    主题

---
+++ lib/MyStore.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'MyStore.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars
+
+mixin _$MyStore on MyStoreBase, Store {
+  final _$darkThemeAtom = Atom(name: 'MyStoreBase.darkTheme');
+
+  @override
+  bool get darkTheme {
+    _$darkThemeAtom.reportObserved();
+    return super.darkTheme;
+  }
+
+  @override
+  set darkTheme(bool value) {
+    _$darkThemeAtom.context
+        .checkIfStateModificationsAreAllowed(_$darkThemeAtom);
+    super.darkTheme = value;
+    _$darkThemeAtom.reportChanged();
+  }
+
+  final _$changeThemeAsyncAction = AsyncAction('changeTheme');
+
+  @override
+  Future changeTheme() {
+    return _$changeThemeAsyncAction.run(() => super.changeTheme());
+  }
+}

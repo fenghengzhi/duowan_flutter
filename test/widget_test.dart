834ab2c1007b703238b2ca6f5fba2da6d7beef81
hzfeng <hzfeng@Ctrip.com>
2019-12-16 16:05:22 +0800

    http

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:duowan_flutter/main.dart';
+import 'package:duowan_flutter/MyApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
